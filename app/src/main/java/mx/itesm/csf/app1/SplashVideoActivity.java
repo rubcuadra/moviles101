57a3cc9d805b5a15986574aa1f08431c4c8bf403
rubcuadra <rubcuadra@gmail.com>
2017-01-26 12:05:41 -0600

    hiding navigation bar

--- app/src/main/java/mx/itesm/csf/app1/SplashVideoActivity.java
+++ app/src/main/java/mx/itesm/csf/app1/SplashVideoActivity.java
@@ -5,28 +5,33 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.VideoView;
 
 public class SplashVideoActivity extends AppCompatActivity {
 
     private VideoView mVideo;
-
+    private static final String TAG = "VideoSplash";
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_video);
 
+        //Hide navigation Bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
         mVideo = (VideoView) findViewById(R.id.splashVideo);
         Uri video = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.ad13);
         mVideo.setVideoURI( video );
+
         mVideo.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
         {
             @Override
             public void onCompletion(MediaPlayer mp)
             {
-                Log.d("ACT1","FINISHED");
                 finish();
             }
         });
