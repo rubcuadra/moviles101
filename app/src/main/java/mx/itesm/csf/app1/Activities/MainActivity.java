48db631d81202eba8debbd29d99721ea004385a0
rubcuadra <rubcuadra@gmail.com>
2017-02-06 16:57:24 -0600

    implementing hw up to 4

--- app/src/main/java/mx/itesm/csf/app1/Activities/MainActivity.java
+++ app/src/main/java/mx/itesm/csf/app1/Activities/MainActivity.java
@@ -1,8 +1,10 @@
 package mx.itesm.csf.app1.Activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +16,10 @@ public class MainActivity extends AppCompatActivity
     private Button secondButton;
     private Button thirdButton;
     private Button fourthButton;
+    private static final int DIAL_ID = 1;
+    private static final String phone = "5591011410";
+    private static final String TAG = "PHONE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,10 +27,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firstButton = (Button) findViewById(R.id.button1);
+        firstButton = (Button) findViewById(R.id.lista);
         secondButton = (Button) findViewById(R.id.button2);
-        thirdButton = (Button) findViewById(R.id.button3);
-        fourthButton = (Button) findViewById(R.id.button4);
+        thirdButton = (Button) findViewById(R.id.llamada);
+        fourthButton = (Button) findViewById(R.id.video);
 
         firstButton.setOnClickListener(new View.OnClickListener()
         {
@@ -49,7 +55,12 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                startActivity(new Intent().setClass(MainActivity.this, Activity3.class));
+                Intent dial = new Intent(Intent.ACTION_DIAL);
+                dial.setData(Uri.parse("tel:"+phone));
+                if (dial.resolveActivity(getPackageManager()) != null)
+                {
+                    startActivityForResult(dial,DIAL_ID);
+                }
             }
         });
 
@@ -62,4 +73,24 @@ public class MainActivity extends AppCompatActivity
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+        switch (requestCode)
+        {
+            case DIAL_ID:
+                if (resultCode == RESULT_OK)
+                {
+                    //Nos dijo que fue exitoso el resultado de la llamada
+                    Log.d(TAG, String.valueOf(data.getData()));
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
 }
