0625f29a86e35437d0c09f7f1cf64fcbf579b8dd
rubcuadra <rubcuadra@gmail.com>
2017-02-06 18:00:04 -0600

    more time and as well implemented native dials

--- app/src/main/java/mx/itesm/csf/app1/Activities/MainActivity.java
+++ app/src/main/java/mx/itesm/csf/app1/Activities/MainActivity.java
@@ -6,7 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import mx.itesm.csf.app1.R;
 
@@ -16,7 +19,14 @@ public class MainActivity extends AppCompatActivity
     private Button secondButton;
     private Button thirdButton;
     private Button fourthButton;
-    private static final int DIAL_ID = 1;
+    private Button fifthButton;
+    private Spinner spinAct;
+
+    private String[] av_activities;
+    private String selectedIntent;
+    private int selectedIntentId;
+
+    private static final int DIAL_ID = 43;
     private static final String phone = "5591011410";
     private static final String TAG = "PHONE";
 
@@ -31,6 +41,10 @@ public class MainActivity extends AppCompatActivity
         secondButton = (Button) findViewById(R.id.button2);
         thirdButton = (Button) findViewById(R.id.llamada);
         fourthButton = (Button) findViewById(R.id.video);
+        fifthButton = (Button) findViewById(R.id.activities);
+        spinAct = (Spinner) findViewById(R.id.activityChoice);
+
+        av_activities = getResources().getStringArray(R.array.activities_array);
 
         firstButton.setOnClickListener(new View.OnClickListener()
         {
@@ -72,6 +86,74 @@ public class MainActivity extends AppCompatActivity
                 startActivity(new Intent().setClass(MainActivity.this, SplashVideoActivity.class));
             }
         });
+        fifthButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                try
+                {
+                    if (selectedIntent != null)
+                        startActivityForResult(new Intent(selectedIntent), selectedIntentId);
+                }
+                catch (Exception e)
+                {
+                    Log.e("LOG",e.toString());
+                    Toast.makeText(MainActivity.this,"No se encontro actividad para eso", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        spinAct.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                fifthButton.setText(av_activities[position]);
+                selectedIntentId = position;
+                switch (position)
+                {
+                    case 0:
+                        selectedIntent = Intent.ACTION_ANSWER;
+                        break;
+                    case 1:
+                        selectedIntent = Intent.ACTION_CALL;
+                        break;
+                    case 2:
+                        selectedIntent = Intent.ACTION_DELETE;
+                        break;
+                    case 3:
+                        selectedIntent = Intent.ACTION_DIAL;
+                        break;
+                    case 4:
+                        selectedIntent = Intent.ACTION_EDIT;
+                        break;
+                    case 5:
+                        selectedIntent = Intent.ACTION_INSERT;
+                        break;
+                    case 6:
+                        selectedIntent = Intent.ACTION_PICK;
+                        break;
+                    case 7:
+                        selectedIntent = Intent.ACTION_SEARCH;
+                        break;
+                    case 8:
+                        selectedIntent = Intent.ACTION_SENDTO;
+                        break;
+                    case 9:
+                        selectedIntent = Intent.ACTION_VIEW;
+                        break;
+                    case 10:
+                        selectedIntent = Intent.ACTION_WEB_SEARCH;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
     }
 
     @Override
@@ -83,7 +165,6 @@ public class MainActivity extends AppCompatActivity
             case DIAL_ID:
                 if (resultCode == RESULT_OK)
                 {
-                    //Nos dijo que fue exitoso el resultado de la llamada
                     Log.d(TAG, String.valueOf(data.getData()));
                 }
                 break;
